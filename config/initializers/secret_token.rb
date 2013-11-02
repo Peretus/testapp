436ab456ff2766952b6b476a3d494b53b21c2801
Casey McNeil <caseymcneil@gmail.com>
2013-11-02 17:25:27 -0500

    My first commit to git with a testapp that has status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testapp::Application.config.secret_key_base = '960bcf8e6a542a3fde9699448401da44f52cfaefb60b9be420b973ffe1b618032a2f7afb8d6581b3a8a01ea0121fd996794cfb2e043a698d35d563ec0cb57fb6'
